235e9c0c9c634afc4693ac8ba1d87026548520bd
Walter <walterxie@users.noreply.github.com>
2016-09-20 12:25:42 +1200

    fix bug on taxa fig

--- R/TaxonomyAnalyses.R
+++ R/TaxonomyAnalyses.R
@@ -111,7 +111,8 @@ plotTaxonomy <- function(all.counts.sums, taxa.ref="", taxa.rank="phylum", group
       stop("Invalid taxonomic reference data set: no ", taxa.rank, " column !")
     z$taxa <- taxa.ref[match(tolower(z$taxa), tolower(taxa.ref[,taxa.rank])), taxa.rank]
   }
-  require(data.table)
+  require(reshape2)
+  z <- as.data.frame(z)
   z <- melt(z, id.vars = c("taxa", "group", "gene"))
   # Order factors
   if (length(taxa.ref) > 0)
@@ -120,19 +121,14 @@ plotTaxonomy <- function(all.counts.sums, taxa.ref="", taxa.rank="phylum", group
   z$group <- gsub("root|No hits|Not assigned|cellular organisms", "Unknown", z$group)
   z$group <- factor(z$group, ordered = TRUE, levels = group.level)
   z <- na.omit(z)
-  
+  #print(z)
   require(ggplot2)
   p <- ggplot(z) + 
-    geom_point(data = z[variable == "count.1"], 
-               aes(x = taxa, y = value, colour = group), shape = 1, size = 2) +
-    geom_point(data = z[variable == "sum.1"], 
-               aes(x = taxa, y = value, colour = group), shape = 5, size = 2, show.legend = NA) +
-    geom_point(data = z[variable == "count.2"], 
-               aes(x = taxa, y = value, colour = group), shape = 16, size = 2) +
-    #geom_point(data = z[variable == "sums.2"], aes(x = taxa, y = value, colour = group), shape = 17, size = 2) +
-    geom_line(data = z, 
-              aes(x = taxa, y = value, group=interaction(taxa, gene), colour = group), 
-              size = 0.5, alpha = 0.5) + 
+    geom_point(data = z[z$variable == "count.1",], aes(x = taxa, y = value, colour = group), shape = 1, size = 2) +
+    geom_point(data = z[z$variable == "sum.1",], aes(x = taxa, y = value, colour = group), shape = 5, size = 2, show.legend = NA) +
+    geom_point(data = z[z$variable == "count.2",], aes(x = taxa, y = value, colour = group), shape = 16, size = 2) +
+    #geom_point(data = z[z$variable == "sums.2",], aes(x = taxa, y = value, colour = group), shape = 17, size = 2) +
+    geom_line(data = z, aes(x = taxa, y = value, group=interaction(taxa, gene), colour = group), size = 0.5, alpha = 0.5) + 
     facet_grid( ~ gene) + guides(fill = guide_legend(reverse = FALSE)) +
     coord_flip() + theme_bw() + ylab(y.lab) + xlab(x.lab) + 
     scale_y_log10(breaks = c(1, 10, 100, 1000, 10000, 100000, 1000000), label=ComMA::scientific_10) +
@@ -160,6 +156,9 @@ plotTaxonomy <- function(all.counts.sums, taxa.ref="", taxa.rank="phylum", group
 #' @rdname countOTUsReads
 sumReadsOTUs <- function(cm.taxa.list, taxa.ref="", taxa.rank="phylum", group.rank="kingdom", 
                          col.ranks=c("kingdom", "phylum", "class", "order"), 
+                         gene.level=c("16S", "18S", "26S", "ITS", "COI-300", "COI-650"),
+                         group.level=c("ARCHAEA","BACTERIA","EUKARYOTA","PROTOZOA","CHROMISTA","FUNGI","PLANTAE","ANIMALIA","Unknown"),
+                         x.lab="Phylum (or higher-level taxon)", y.lab="Number of sequences or OTUs",
                          fig.folder="./figures", table.folder="./outputs"){
   all.counts.sums <- ComMA::getCountSums(cm.taxa.list, input.list=T, taxa.rank=taxa.rank, group.rank=group.rank)
   
@@ -168,7 +167,8 @@ sumReadsOTUs <- function(cm.taxa.list, taxa.ref="", taxa.rank="phylum", group.ra
                 sep = "\t", quote = FALSE, col.names = NA)
   }
   if (!is.na(fig.folder)) {
-    p <- ComMA::plotTaxonomy(all.counts.sums)
+    p <- ComMA::plotTaxonomy(all.counts.sums, taxa.ref=taxa.ref, gene.level=gene.level, 
+                             group.level=group.level, x.lab=x.lab, y.lab=y.lab)
     ggsave(p, file = file.path(fig.folder, paste0("Overall_taxonomy_OTUs_reads_by_", taxa.rank, ".pdf")), 
            width = 260, height = 200, units = "mm")
   }
