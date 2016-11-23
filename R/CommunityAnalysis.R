63fe2ab9c1b43a317e6d60d053d7cfca4019f637
Walter <walterxie@users.noreply.github.com>
2016-11-23 16:36:23 +1300

    add ctree

--- R/CommunityAnalysis.R
+++ R/CommunityAnalysis.R
@@ -1,16 +1,49 @@
 
-#' @title
-#' Classify sample attribute by taxonomic composition
+#' @name classification
+#' @title Classify samples by taxonomic composition
+#'
+#' @description
+#' Build a classification model to classify or predict 
+#' the certain type of samples (e.g. land types).
 #' 
 #' @details
+#' Create a conditional inference tree \code{\link{ctree}} for the classification of samples.
+#' Ctree uses a significance test procedure in order to select variables 
+#' instead of selecting the variable that maximizes an information measure (e.g. Gini coefficient).
+#' \url{http://stats.stackexchange.com/questions/12140/conditional-inference-trees-vs-traditional-decision-trees}.
+#' 
+#' @param comm A community matrix, which can be either abundance or relative abundance.
+#' @param levels Levels to order data by \code{group.id}.
+#' @keywords classification
+#' @export
+#' @examples 
+#' model <- ctreeClassification(comm, attr.data=env, group.id="land.use")
+#' plot(model$ctree)
+#' 
+#' @rdname classification
+ctreeClassification <- function(comm, attr.data, group.id, levels=c(), ...) {
+  require(party)
+  input.dat <- as.data.frame(t(comm))
+  input.dat[,group.id] <- attr.data[match(rownames(input.dat), rownames(attr.data)), group.id]
+  if (length(levels) < 1)
+    levels <- sort(unique(input.dat[,group.id]))
+  input.dat[,group.id] <- factor(input.dat[,group.id], levels=levels)
+  
+  cat("Input community has", nrow(input.dat), "samples, and the target", 
+      group.id, "has", length(unique(input.dat[,group.id])), "categories.")
+  
+  output.tree <- ctree(as.formula(paste(group.id, ".", sep=" ~ ")), data = input.dat, ...)
+  return(ctree=output.tree, data=input.dat, levels=levels)
+}
+
+#' @details
 #' Build a classification model using lasso \code{\link{cv.glmnet}},
-#' to predict the certain type of samples (e.g. land types) 
 #' given relative abundance of taxonomic compositions (e.g. families) in the samples, 
-#' and also to find which taxonomic compositions are important to express this model.
+#' and also to find which taxonomic compositions are important.
 #' 
-#' This can be used after 
-#' 
-#' @param data,attr.data,group.id,percent Refer to \code{\link{enterotype}}.
+#' @param relative.abund,attr.data,percent Refer to \code{\link{enterotype}}.
+#' @param group.id The column name in \code{attr.data} contains the known groups to 
+#' compare with enterotypes.
 #' @param alpha,family,nlambda,... Refer to \code{\link{cv.glmnet}}.
 #' @param coef.s Which coeffient to return by \code{\link{coef}}.
 #' \code{lambda.min}, as default, is the value of lambda that 
@@ -20,13 +53,15 @@
 #' @keywords classification
 #' @export
 #' @examples 
-#' cvfit <- classifyByTaxaComp(abun.dist.matrix, attr.data=env, group.id="land.use")
-classifyByTaxaComp <- function(data, attr.data, group.id, percent=0.01, 
-                               alpha=1, family='multinomial', nlambda = 500, 
-                               coef.s = "lambda.min", return.df=TRUE, ...) {
+#' cvfit <- lassoClassification(relative.abund, attr.data=env, group.id="land.use")
+#' 
+#' @rdname classification
+lassoClassification <- function(relative.abund, attr.data, group.id, percent=0.01, 
+                                alpha=1, family='multinomial', nlambda = 500, 
+                                coef.s = "lambda.min", return.df=TRUE, ...) {
   # remove noise
-  data <- noise.removal(data, percent=percent)
-  x <- t(data)
+  relative.abund <- noise.removal(relative.abund, percent=percent)
+  x <- t(relative.abund)
   y <- attr.data[match(rownames(x), rownames(attr.data)), group.id]
   
   if (nrow(x) != length(y))
@@ -46,3 +81,4 @@ classifyByTaxaComp <- function(data, attr.data, group.id, percent=0.01,
   
   list(fit=cvfit, model=model, return.df=return.df)
 }
+
